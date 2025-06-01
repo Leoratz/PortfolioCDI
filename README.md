3f8f5c9f6dc50c744e9fac8d634f082d5b853e31
Léora Chriqui <leora.chriqui@gmail.com>
2025-06-01 23:05:56 +0200

    README and database

--- README.md
+++ README.md
@@ -40,3 +40,48 @@ La première étape est d'importer les utilisateurs de base avec la commande `ph
 Suite à cette commande, un nouvel utilisateur apparaitra dans votre base de données : un administrateur, avec l'email admin@email.fr, et le rôle d'administrateur (ROLE_ADMIN).
 
 Il est possible de se connecter aux profils de cet utilisateur avec son adresse mail, et le mot de passe "admin".
+
+### 2. Ajouter des projets de bases
+
+Ensuite, il vous est conseillé d'importer dans votre base de données le jeu de données au format SQL présent ici (`portfoliocdi.sql`).
+
+## Découverte du projet
+
+Vous pouvez maintenant librement profiter de ce projet. Vous trouverez ci-dessous des parcours-type détaillés.
+
+### 1. Parcours visiteur
+
+Vous arrivez sur la page de base, et regardez les différents projets. L'un d'entre-eux vous intéresse, et vous cliquez sur le bouton "En savoir plus" afin de consulter la page du projet.
+Vous trouvez sur celle-ci des informations relatives au projet.
+
+Après avoir consulté tous les projets ayant capté votre attention, vous décidez de laisser vos contacts afin de demander des informations sur les modalités d'inscription au sein de l'école.
+Pour cela, vous allez dans le footer et complétez et envoyez le formulaire.
+
+### 2. Parcours administrateur
+
+Vous arrivez sur la page de base, et cliquez sur 'Connexion'. Sur la page de login, vous remplissez vos informations de connexion (`admin@email.fr` pour l'email et `admin` pour le mot de passe par exemple). 
+
+Une fois connecté, vous cliquez sur l'onglet 'Utilisateurs' puis sur 'Ajouter un administrateur' pour créer un nouvel utilisateur simple. Vous remplissez le formulaire puis le soummetez.
+
+Vous allez sur la page des projets, et mettez le premier projet comme invisible.
+
+Vous cliquez sur le bouton 'déconnection' de la navbar.
+
+## 3. Parcours utilisateur
+
+Vous arrivez sur la page de base, et cliquez sur 'Connexion'. Sur la page de login, vous remplissez vos informations de connexion (avec les identifiants créés par l'administrateur précédemment par exemple). 
+
+Vous allez sur la page des projets et modifiez le titre de l'un des projets.
+
+Ensuite, vous cliquez sur l'onglet 'Demande de contacts'.
+Vous sélectionnez l'une des demandes avec une bordure verte, et la modifiez afin de passer son état en 'En cours'.
+Vous sélectionnez une autre à la bordure rouge, et cliquez sur la poubelle pour la supprimer.
+
+Vous cliquez sur le bouton 'déconnection' de la navbar.
+
+## Informations supplémentaires
+
+Un invité ne peut que remplir le formulaire de demande de contact, et n'a pas accès aux pages de création, modification et suppression des divers éléments (projets, demandes de contact, utilisateurs).
+
+Un utilisateur peut créer des projets, les rendre invisible, et les supprimer. Il peut également modifier l'étape de traitement d'une demande de contact, ainsi que la supprimer.
+Un administateur possède les mêmes droits qu'un utilisateur simple, en plus de pouvoir créer, modifier et supprimer des utilisateurs (ce que les utilisateurs et les invités ne peuvent pas faire).
---
+++ portfoliocdi.sql
@@ -0,0 +1,293 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : dim. 01 juin 2025 à 21:04
+-- Version du serveur : 8.0.30
+-- Version de PHP : 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `portfoliocdi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+--
+-- Déchargement des données de la table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20250524144355', '2025-05-29 22:07:08', 132),
+('DoctrineMigrations\\Version20250528191747', '2025-05-29 22:07:08', 28);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `guest`
+--
+
+CREATE TABLE `guest` (
+  `id` int NOT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `details` longtext COLLATE utf8mb4_unicode_ci,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `guest`
+--
+
+INSERT INTO `guest` (`id`, `last_name`, `first_name`, `email`, `details`, `status`) VALUES
+(3, 'Hysse', 'Paul', 'paul.hysse@test.com', 'Bonjour,\nLes programmes proposés par votre école m\'intéressent beaucoup. Serait-ce possible d\'en savoir plus ?\nMerci', 'pending'),
+(4, 'Viation', 'Alyssia', 'alyssia.vation@test.com', 'Bonjour votre école a l\'air super !', 'done'),
+(5, 'Dodo', 'Léo', 'leo.dodo@test.com', 'C\'est combien l\'année ?', 'onGoing');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `media`
+--
+
+CREATE TABLE `media` (
+  `id` int NOT NULL,
+  `project_id` int DEFAULT NULL,
+  `file_path` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `media`
+--
+
+INSERT INTO `media` (`id`, `project_id`, `file_path`) VALUES
+(7, NULL, 'wallpaperflare-com-wallpaper-68397608ef9fa209259108.jpg'),
+(10, NULL, 'the-rolling-stones-logo-68399753a5c8b840037483.png'),
+(13, NULL, 'iim-683c7029a3650343033743.png'),
+(14, NULL, 'wallpaperflare-com-wallpaper-683c705e6755c019578481.jpg'),
+(15, NULL, 'wallpaper3-683c72c0a6eb7328475191.jpg'),
+(25, NULL, 'wallpaper3-683cb750da608890984509.jpg'),
+(29, 22, 'arton103809-683cbb70a9097767736533.png'),
+(30, 22, 'visuel-api-683cbb71053b1423265505.png'),
+(31, NULL, 'images22-683cbc5d6b13f298199981.jpg'),
+(32, 23, 'images22-683cbccbe4b0f656088089.jpg'),
+(33, 24, 'github-683cbdd86a06e021886833.png');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `project`
+--
+
+CREATE TABLE `project` (
+  `id` int NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `details` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `year` int NOT NULL,
+  `stack` longtext COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '(DC2Type:array)',
+  `link` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `visibility` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `project`
+--
+
+INSERT INTO `project` (`id`, `title`, `details`, `year`, `stack`, `link`, `visibility`) VALUES
+(22, 'Création d\'une API', 'Ceci est un projet recensant la creation dune API', 1, 'a:2:{i:0;s:3:\"php\";i:1;s:7:\"symfony\";}', '', 1),
+(23, 'Qu\'est ce que le développement', 'Réponse à la question : le développement c\'est quoi', 3, 'a:2:{i:0;s:2:\"js\";i:1;s:3:\"php\";}', '', 1),
+(24, 'Projet à venir', 'Ce projet est invisible pour les visiteurs de notre site', 1, 'a:1:{i:0;s:4:\"java\";}', '', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `project_student`
+--
+
+CREATE TABLE `project_student` (
+  `project_id` int NOT NULL,
+  `student_id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `project_student`
+--
+
+INSERT INTO `project_student` (`project_id`, `student_id`) VALUES
+(22, 5),
+(22, 6),
+(23, 6),
+(24, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `student`
+--
+
+CREATE TABLE `student` (
+  `id` int NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `github` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `student`
+--
+
+INSERT INTO `student` (`id`, `first_name`, `last_name`, `github`) VALUES
+(4, 'Marie', 'Olet', 'https://github.com/'),
+(5, 'Paul', 'Mirabelle', 'https://github.com/'),
+(6, 'Jean', 'Darme', 'https://github.com/');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` json NOT NULL COMMENT '(DC2Type:json)',
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `first_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `roles`, `password`, `last_name`, `first_name`) VALUES
+(1, 'user@example.com', '[\"ROLE_ADMIN\"]', '$2y$13$wQMczqmR5EpTXqHQlMuce.VhuIEu4wheoN9DDX0KJibUY/TbB29AS', 'test', 'test'),
+(2, 'another@example.com', '[\"ROLE_ADMIN\"]', '$2y$13$yMj6kRjscI2uXYmdlyr4fOwfy2QV3CZMGQtCZGWySMY84LISP4hoG', 'test', 'test'),
+(5, 'again@example.com', '[]', '$2y$13$plveBZQvpwfFUOdHdOgDbOf./TtKVl8xrRuhVacUkUH5Iv6PhOUHG', 'test', 'test');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Index pour la table `guest`
+--
+ALTER TABLE `guest`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `media`
+--
+ALTER TABLE `media`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_6A2CA10C166D1F9C` (`project_id`);
+
+--
+-- Index pour la table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `project_student`
+--
+ALTER TABLE `project_student`
+  ADD PRIMARY KEY (`project_id`,`student_id`),
+  ADD KEY `IDX_213DA356166D1F9C` (`project_id`),
+  ADD KEY `IDX_213DA356CB944F1A` (`student_id`);
+
+--
+-- Index pour la table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_IDENTIFIER_EMAIL` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `guest`
+--
+ALTER TABLE `guest`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `media`
+--
+ALTER TABLE `media`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT pour la table `project`
+--
+ALTER TABLE `project`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT pour la table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `media`
+--
+ALTER TABLE `media`
+  ADD CONSTRAINT `FK_6A2CA10C166D1F9C` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `project_student`
+--
+ALTER TABLE `project_student`
+  ADD CONSTRAINT `FK_213DA356166D1F9C` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_213DA356CB944F1A` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
